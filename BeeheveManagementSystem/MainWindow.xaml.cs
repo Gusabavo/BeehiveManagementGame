de133ebe22891aa3d846b7d3e3bbf69fea8391ef
Gusabavo <gustavo.sk.caxias@gmail.com>
2024-05-23 17:41:10 -0300

    Classes added.

--- BeeheveManagementSystem/MainWindow.xaml.cs
+++ BeeheveManagementSystem/MainWindow.xaml.cs
@@ -8,17 +8,39 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace BeeheveManagementSystem
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer = new DispatcherTimer();
+        private Queen queen = new Queen();
+
         public MainWindow()
         {
             InitializeComponent();
+            statusReport.Text = queen.StatusReport;
+            timer.Tick += Timer_Tick;
+            timer.Interval = TimeSpan.FromSeconds(2);
+            timer.Start();
         }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            WorkShift_Click(this, new RoutedEventArgs());
+        }
+        private void WorkShift_Click(object sender, RoutedEventArgs e)
+        {
+            queen.WorkTheNextShift();
+            statusReport.Text = queen.StatusReport;
+        }
+
+        private void AssignJob_Click(object sender, RoutedEventArgs e)
+        {
+            queen.AssignBee(jobSelector.Text);
+            statusReport.Text = queen.StatusReport;
+        }
+
+
     }
 }
